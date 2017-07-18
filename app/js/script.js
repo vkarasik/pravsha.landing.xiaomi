d93c545f70b8f2f52e4d56af3e1abd8b7a3ff9d9
Vadim Karasik <vadkarasik@gmail.com>
2017-07-18 21:46:55 +0300

    five commit

--- app/js/script.js
+++ app/js/script.js
@@ -14,17 +14,54 @@ $(function(){
 });
 
 $(function(){
-    var miModels = ['Mi Max','Mi5 High','Mi 4s'] // Модели из серии
-    $('#mi,#redmi,#redmi-note,#hongmi').click(function(){
+    var mi = ['Mi5 High',
+            'Mi 4s',
+            'Mi 5 Pro',
+            'MiPad 2',
+            'Mi4c',
+            'Mi4i',
+            'Mi Note Pro',
+            'Mi Note',
+            'Mi4 LTE',
+            'Mi 6',
+            'Mi 5c',
+            'Mi Note 2',
+            'Mi Max Prime',
+            'Mi 5s Plus',
+            'Mi 5s'] // Модели из серии mi
+    
+    var redmi = ['Redmi 3X',
+            'Redmi Pro',
+            'Redmi 3S Pro',
+            'Redmi 4 SE',
+            'Redmi 4 HE',
+            'Redmi 4A',
+            'Redmi 2A',
+            'Redmi 2 Prime',
+            'Redmi Note 2',
+            'Redmi 2 Pro',
+            'Redmi 3'] // Модели из серии redmi
+    
+    var redminote = ['Redmi Note 2 Prime',
+            'Redmi Note 4G Dual SIM',
+            'Redmi Note 4',
+            'Redmi Note 4х']
+    
+    var hongmi = ['Hongmi 1S 4G','Hongmi 1S 3G']
+
+    $('#mi,#redmi,#redminote,#hongmi').click(function(el){
         
-        var test = $(this).attr('id');
-        console.log(test);
-        $('.series span').remove();
+        console.log(el);
         
-        for(i=0;i<miModels.length;++i){
-            $('.series').append('<span>' + miModels[i] + '</span>').hide();
+        var filterID = $(this).attr('id');
+        var modelsArr = eval(filterID); // получим ссылку на массив моделей текущего отбора
+        $('.series span').remove(); // очистим предыдущие результаты отбора
+        
+        for(i=0; i<modelsArr.length; ++i){
+            $('.series').append('<span>' + modelsArr[i] + '</span>').hide();
         }
         $('.series').slideDown();
-        
+        $(this).addClass('activ');
+        $('.series-filter span').not(this).removeClass('activ');
     })
 });
